c50b6341f5267c4394b2092cac1aa7f44a14bb0c
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 16:00:04 -0500

    [js] Fill out constructor for Pokedex class

---
+++ src/components/Pokedex.js
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+
+class Pokedex extends Component {
+    constructor() {
+        super()
+        this.state = {
+            pokemonName: 'pikachu'
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                Text
+            </div>
+        )
+    }
+}
+
+export default Pokedex
